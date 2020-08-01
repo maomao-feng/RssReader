b79f08bf5c103df4d9fbe1c775a1dc608d4ea5be
Feng <mfMM1565@gmail.com>
2020-08-01 14:29:36 +0900

    first commit: project initialized

---
+++ src/main/java/rssreader/RssReader.java
@@ -0,0 +1,11 @@
+package rssreader;
+
+public class RssReader {
+    public String greet() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RssReader().greet());
+    }
+}
---
+++ src/test/java/rssreader/RssReaderTest.java
@@ -0,0 +1,14 @@
+package rssreader;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RssReaderTest {
+    @Test
+    public void test() {
+        RssReader rssReader = new RssReader();
+        assertThat(rssReader.greet(), is("Hello world."));
+    }
+}
