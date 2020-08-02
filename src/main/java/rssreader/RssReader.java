a58b484a0d4c4d097e1534e23e497a73e4a1825d
Feng <mfMM1565@gmail.com>
2020-08-02 16:28:31 +0900

    delegate to ContentProcessor after parsing command to config

--- src/main/java/rssreader/RssReader.java
+++ src/main/java/rssreader/RssReader.java
@@ -1,19 +1,20 @@
 package rssreader;
 
+import articlefetcher.FileFetcher;
+import articlefetcher.RssFetcher;
 import picocli.CommandLine;
 
 public class RssReader{
 
     public static void main(String[] args) {
         CommandLine commandLine = new CommandLine(new CommandProcessor());
-    	int exitCode = commandLine.execute(args);
+
+        int exitCode = commandLine.execute(args);
         if (exitCode != 0)
         	System.exit(exitCode);
+
         ReaderConfig config = commandLine.getExecutionResult();
-		System.out.println("source: " + config.sourcePath());
-		System.out.println("convert: ");
-		config.convertWays().forEach(e -> System.out.println(e));
-		System.out.println("output file: " + config.outputFileName());
+        new ContentProcessor(new RssFetcher(), new FileFetcher()).process(config);
     }
 
 }
